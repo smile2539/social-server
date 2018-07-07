b8d397a626a89910facc75ca22de1cd6695709c8
Yupaporn Chaiwut <mindmindyupaporn@gmail.com>
2018-07-07 10:47:46 +0700

    social-server

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,10 @@
 const app = {
   db: {
-    host: "localhost",
-    port: 27017,
-    name: "social",
-    username: "",
-    password: ""
+    host: "ds129166.mlab.com",
+    port: 29166,
+    name: "server",
+    username: "admin",
+    password: "yupaporn2539"
   },
   jwt: {
     APP_SECRET: "ILOVEYOU",
